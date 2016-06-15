f65d70c51185d0b6ec1ecebfb8a30ccf0242a586
kavikat <kavikaat@gmail.com>
2016-06-14 22:58:01 -0700

    first push

---
+++ js/main.js
@@ -0,0 +1,36 @@
+ var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '500',
+          width: '800',
+          videoId: '11r-ziOS9a0',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          //setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+      function zoom() {
+       player.setSize(width=100, height=100);
+      }
+      document.getElementById('playerHolder').addEventListener('mouseover', zoom, false);
